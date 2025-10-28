4de3b5cf117e900f11f8c46ee77cc7a645217bad
Jacob Handwerger <jstudyeth@gmail.com>
2025-10-28 15:18:52 -0400

    palma

--- lib/projects-data.ts
+++ lib/projects-data.ts
@@ -1,20 +1,20 @@
 export const projects = [
   {
-    title: "Leigh Rothschild",
-    category: "Personal Website",
-    mainImage: "/leigh.jpg",
-    website: "https://leighmrothschild.com"
+    title: 'Leigh Rothschild',
+    category: 'Personal Website',
+    mainImage: '/leigh.jpg',
+    website: 'https://leighmrothschild.com',
   },
-    {
-    title: "Livana Soul Doula",
-    category: "Personal Website",
-    mainImage: "/livana.jpg",
-    website: "https://livanasouldoula.com"
+  {
+    title: 'Palma Miami Beach',
+    category: 'Business Website',
+    mainImage: '/palma.jpg',
+    website: 'https://palmamiamibeach.com',
   },
-    {
-    title: "Chaya Adamson",
-    category: "Personal Website",
-    mainImage: "/chaya.jpg",
-    website: "https://chayaadamson.com"
+  {
+    title: 'Livana Soul Doula',
+    category: 'Personal Website',
+    mainImage: '/livana.jpg',
+    website: 'https://livanasouldoula.com',
   },
-]
+];
